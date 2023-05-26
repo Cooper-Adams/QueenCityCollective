e4ea8c080463fdbffb9b1ed84adea9434dad5ffe
Cooper-Adams <cooperadamsbusiness@gmail.com>
2023-05-26 02:20:40 -0400

    Updated the functions to properly dynamically add and remove tags as well as changing the naming scheme for POSTing

--- public/adminFunctions.js
+++ public/adminFunctions.js
@@ -1,18 +1,44 @@
 const newBtn = document.getElementById("newTagBtn")
 const closeBtn = document.querySelector("[data-close-modal]")
 const submitBtn = document.querySelector("[data-submit-modal]")
+const deleteBtns = document.querySelectorAll(".tagBtn")
 const modal = document.querySelector("[data-modal]")
 const input = document.querySelector(".modalInput")
+const tags = document.getElementById("tags")
 
 newBtn.addEventListener("click", () => { modal.showModal() })
 
 closeBtn.addEventListener("click", () => { modal.close() })
 
 submitBtn.addEventListener("click", () => {
-    //Add a new tag div with the input.value
+    const newTag = document.createElement("div")
+    newTag.classList.add("tag")
+
+    const newTagBtn = document.createElement("button")
+    newTagBtn.classList.add("tagBtn")
+    newTagBtn.textContent = '✖';
+    newTagBtn.setAttribute('type', "button")
+    newTagBtn.setAttribute('id', 'tagBtn')
+    newTagBtn.addEventListener("click", (e) => {
+        tags.removeChild(e.target.parentElement)
+    })
+
+    const newTagInput = document.createElement("input")
+    newTagInput.setAttribute('type', "text")
+    newTagInput.setAttribute('value', input.value)
+    newTagInput.setAttribute('name', "tags[]")
+
+    newTag.appendChild(newTagInput)
+    newTag.appendChild(newTagBtn)
+
+    tags.appendChild(newTag)
 
     modal.close()
 })
+
+deleteBtns.forEach(() => addEventListener("click", (e) => {
+    tags.removeChild(e.target.parentElement)
+}))
 
 modal.addEventListener("click", e => {
     const modalDimensions = modal.getBoundingClientRect()
