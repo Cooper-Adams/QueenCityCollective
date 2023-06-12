94fa8f8c5972a68ed4977c041eac21b47f50939d
Cooper-Adams <cooperadamsbusiness@gmail.com>
2023-06-12 14:05:05 -0400

    Updated the POST route to properly configure the article with sanitized HTML (allows iframes)

--- routes/articles.js
+++ routes/articles.js
@@ -3,6 +3,10 @@ const Article = require('./../models/article')
 const { MongoClient } = require('mongodb')
 const { ObjectID } = require('bson')
 const slugify = require('slugify')
+const marked = require('marked')
+const createDomPurifier = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurifier(new JSDOM().window)
 const router = express.Router()
 
 var dotenv = require('dotenv').config()
@@ -20,24 +24,32 @@ router.get('/:slug', async (req, res) => {
     await client.close();
 })
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown,
-        createdAt: req.body.createdAt,
-        updatedAt: req.body.updatedAt,
-        tags: req.body['tags[]'],
-        author: req.body.author,
-        category: req.body.category,
-        published: req.body.truefalse,
-        slug: slugify(req.body.title, { lower: true, strict: true })
-    })
-    
+router.post('/', async (req, res) => {    
     try {
         const client = new MongoClient(url)
 
-        await client.db("QCC-DB").collection("Articles").insertOne(article);
+        let newObjectId
+        if (req.body._id == null) { newObjectId = new ObjectID() }
+        else {newObjectId = ObjectID(req.body._id)}
+
+        await client.db("QCC-DB").collection("Articles").replaceOne(
+            {_id: newObjectId},
+            {
+                _id: newObjectId,
+                title: req.body.title,
+                description: req.body.description,
+                markdown: req.body.markdown,
+                createdAt: Date(req.body.createdAt),
+                updatedAt: Date(req.body.updatedAt),
+                tags: req.body['tags[]'],
+                author: req.body.author,
+                category: req.body.category,
+                published: req.body.truefalse,
+                slug: slugify(req.body.title, { lower: true, strict: true }),
+                sanitizedHtml: dompurify.sanitize(marked.parse(req.body.markdown), { ADD_TAGS: ["iframe"], ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling'] })
+            }, 
+            {upsert: true}
+        )
 
         if (req.body.published) {
             res.render(`/articles/${article.slug}`)
