2ce5623db16e34cdb128ba8b9d90b68464c066da
Cooper-Adams <cooperadamsbusiness@gmail.com>
2023-06-13 00:51:48 -0400

    Updated the POST route again to use the article model so it can be passed back into the edit page when there is an error on submission

--- routes/articles.js
+++ routes/articles.js
@@ -6,6 +6,7 @@ const slugify = require('slugify')
 const marked = require('marked')
 const createDomPurifier = require('dompurify')
 const { JSDOM } = require('jsdom')
+const article = require('./../models/article')
 const dompurify = createDomPurifier(new JSDOM().window)
 const router = express.Router()
 
@@ -32,22 +33,24 @@ router.post('/', async (req, res) => {
         if (req.body._id == null) { newObjectId = new ObjectID() }
         else {newObjectId = ObjectID(req.body._id)}
 
+        let article = new Article({
+            _id: newObjectId,
+            title: req.body.title,
+            description: req.body.description,
+            markdown: req.body.markdown,
+            createdAt: req.body.createdAt,
+            updatedAt: req.body.updatedAt,
+            tags: req.body['tags[]'],
+            author: req.body.author,
+            category: req.body.category,
+            published: req.body.truefalse,
+            slug: slugify(req.body.title, { lower: true, strict: true }),
+            sanitizedHtml: dompurify.sanitize(marked.parse(req.body.markdown), { ADD_TAGS: ["iframe"], ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling'] })
+        })
+
         await client.db("QCC-DB").collection("Articles").replaceOne(
             {_id: newObjectId},
-            {
-                _id: newObjectId,
-                title: req.body.title,
-                description: req.body.description,
-                markdown: req.body.markdown,
-                createdAt: Date(req.body.createdAt),
-                updatedAt: Date(req.body.updatedAt),
-                tags: req.body['tags[]'],
-                author: req.body.author,
-                category: req.body.category,
-                published: req.body.truefalse,
-                slug: slugify(req.body.title, { lower: true, strict: true }),
-                sanitizedHtml: dompurify.sanitize(marked.parse(req.body.markdown), { ADD_TAGS: ["iframe"], ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling'] })
-            }, 
+            { _id: article._id, title: article.title, description: article.description, markdown: article.markdown, createdAt: article.createdAt, updatedAt: article.updatedAt, tags: article.tags, author: article.author, category: article.category, published: article.published, slug: article.slug, sanitizedHtml: article.sanitizedHtml}, 
             {upsert: true}
         )
 
@@ -64,7 +67,7 @@ router.post('/', async (req, res) => {
         await client.close()
     } catch (e) {
         console.error(e)
-        res.render('articles/new', { article: article })
+        res.render('admin/edit', { article: article })
     }
 })
 
