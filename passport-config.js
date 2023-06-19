0dfbca30a8ef69b6d9e0ed1e025f410c94c5cd79
Cooper-Adams <cooperadamsbusiness@gmail.com>
2023-06-19 17:17:16 -0400

    Modified the passport config to await responses for serialization

--- passport-config.js
+++ passport-config.js
@@ -3,14 +3,14 @@ const bcrypt = require('bcrypt')
 
 function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => {
-        const user = getUserByEmail(email)
+        const user = await getUserByEmail(email)
 
-        if (user == null) {//CHECK DATABASE?
+        if (user == null) {
             return done(null, false, {message: 'Email does not exist.'})
         }
 
         try {
-            if (await bcrypt.compare(password, user.password /*CHECK DATABASE?*/)) {
+            if (await bcrypt.compare(password, user.password)) {
                 return done(null, user)
             }
 
@@ -23,9 +23,10 @@ function initialize(passport, getUserByEmail, getUserById) {
     }
 
     passport.use(new LocalStrategy({ usernameField: 'email'}, authenticateUser))
-
     passport.serializeUser((user, done) => done(null, user.id))
-    passport.deserializeUser((id, done) => done(null, getUserById(id)))
+    passport.deserializeUser(async (id, done) => {
+        return done(null, await getUserById(id))
+    })
 }
 
 module.exports = initialize
