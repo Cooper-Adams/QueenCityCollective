c9a38e9f33c279d3a214eb507838388e46edf3c0
Cooper-Adams <cooperadamsbusiness@gmail.com>
2023-07-12 17:52:18 -0400

    Added a new passport strategy for Twitter authentication

--- passport-config.js
+++ passport-config.js
@@ -1,5 +1,51 @@
-const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
+const passport  = require('passport')
+const {MongoClient} = require('mongodb')
+const UserModel = require('./models/userModel')
+var dotenv = require('dotenv').config()
+
+const LocalStrategy = require('passport-local').Strategy
+const { Strategy } = require('@superfaceai/passport-twitter-oauth2');
+
+//Twitter Strategy
+passport.use(
+    new Strategy(
+      {
+        clientID: process.env.TWITTER_CLIENT_ID,
+        clientSecret: process.env.TWITTER_CLIENT_SECRET,
+        clientType: 'confidential',
+        callbackURL: `${process.env.BASE_URL}/auth/twitter/callback`,
+      },
+      async (access_token_key, refreshToken, profile, done) => {
+        const client = new MongoClient(process.env.MONGOLAB_URL)
+
+        const currentUser = await client.db("QCC-DB").collection("Profiles").findOne({id: profile.id})
+
+        if (!currentUser) {
+            const hashedPassword = await bcrypt.hash(profile.id, 10)
+
+            const newUser = new UserModel({
+                id: profile.id,
+                email: "DEFAULT",
+                firstName: "TWITTER",
+                lastName: "USER",
+                screenName: profile.displayName,
+                password: hashedPassword
+            })
+
+            await client.db("QCC-DB").collection("Profiles").insertOne(newUser)
+
+            if (newUser) {
+                return done(null, newUser)
+            }
+        }
+
+        await client.close()
+
+        return done(null, currentUser);
+      }
+    )
+)
 
 function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => {
