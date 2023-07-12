3285fe34439d30bbd98d55388592c2f04d6bbac0
Cooper-Adams <cooperadamsbusiness@gmail.com>
2023-07-12 17:53:08 -0400

    Added a timer on the hidden class for the flash messages.

--- public/logRegFunctions.js
+++ public/logRegFunctions.js
@@ -1,6 +1,7 @@
 const signUpButton = document.getElementById('signUp')
 const signInButton = document.getElementById('signIn')
 const container = document.getElementById('container')
+const errorDiv = document.querySelector('.error-div')
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active")
@@ -9,3 +10,7 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active")
 })
+
+setTimeout(function() {
+	errorDiv.classList.add('hidden')
+}, 2000);
