f90c181b16fe2171a4e2e4c0057770596b920a0e
Cooper-Adams <cooperadamsbusiness@gmail.com>
2023-07-14 15:35:27 -0400

    Modularized the articles router so that it only contains routes that are relevant to displaying article information (for users)

--- routes/articles.js
+++ routes/articles.js
@@ -1,62 +1,79 @@
 const express = require('express')
-const Article = require('./../models/article')
-const { MongoClient } = require('mongodb')
-const { ObjectID } = require('bson')
-const slugify = require('slugify')
-const marked = require('marked')
-const createDomPurifier = require('dompurify')
-const { JSDOM } = require('jsdom')
-const article = require('./../models/article')
-const dompurify = createDomPurifier(new JSDOM().window)
 const router = express.Router()
+const { MongoClient } = require('mongodb')
 
-var dotenv = require('dotenv').config()
-var url = process.env.MONGOLAB_URL
-
-router.post('/', async (req, res) => {    
+router.get('/', async (req, res) => {
     try {
-        const client = new MongoClient(url)
-
-        let newObjectId
-        if (req.body._id == null) { newObjectId = new ObjectID() }
-        else { newObjectId = ObjectID(req.body._id) }
-
-        let article = new Article({
-            _id: newObjectId,
-            title: req.body.title,
-            description: req.body.description,
-            markdown: req.body.markdown,
-            createdAt: req.body.createdAt,
-            updatedAt: req.body.updatedAt,
-            tags: req.body['tags[]'],
-            author: req.body.author,
-            category: req.body.category,
-            published: req.body.truefalse,
-            slug: slugify(req.body.title, { lower: true, strict: true }),
-            sanitizedHtml: dompurify.sanitize(marked.parse(req.body.markdown), { ADD_TAGS: ["iframe"], ADD_ATTR: ['allow', 'allowfullscreen', 'frameborder', 'scrolling'] })
-        })
-
-        await client.db("QCC-DB").collection("Articles").replaceOne(
-            {_id: newObjectId},
-            { _id: article._id, title: article.title, description: article.description, markdown: article.markdown, createdAt: article.createdAt, updatedAt: article.updatedAt, tags: article.tags, author: article.author, category: article.category, published: article.published, slug: article.slug, sanitizedHtml: article.sanitizedHtml}, 
-            {upsert: true}
-        )
-
-        if (req.body.published) { res.render(`/articles/${article.slug}`) }
-
-        else { res.redirect('/adminArticles') }
-
-        await client.close()
+        const client = new MongoClient(process.env.MONGOLAB_URL)
+        const articles = await client.db("QCC-DB").collection("Articles").find().limit(12).sort({createdAt: -1}).toArray();
+        res.render('articles/index', { articles: articles, loggedIn: checkLoggedIn(req.user) })
     } catch (e) {
         console.error(e)
-        res.render('admin/edit', { article: article })
     }
 })
 
-router.delete('/:id', async (req, res) => {
-    const client = new MongoClient(url)
-    await client.db("QCC-DB").collection("Articles").findOneAndDelete({_id: ObjectID(req.params.id)})
-    res.redirect('/adminArticles')
+router.get('/Panthers', async(req, res) => {
+    try {
+        const client = new MongoClient(process.env.MONGOLAB_URL)
+        const articles = await client.db("QCC-DB").collection("Articles").find({category: "Panthers"}).sort({createdAt: -1}).toArray();
+        res.render('articles/Panthers', { articles: articles, loggedIn: checkLoggedIn(req.user) })
+        await client.close()
+    } catch (e) {
+        console.error(e)
+    }
 })
+
+router.get('/Hornets', async(req, res) => {
+    try {
+        const client = new MongoClient(process.env.MONGOLAB_URL)
+        const articles = await client.db("QCC-DB").collection("Articles").find({category: "Hornets"}).sort({createdAt: -1}).toArray();
+        res.render('articles/Hornets', { articles: articles, loggedIn: checkLoggedIn(req.user) })
+        await client.close()
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+router.get('/CharlotteFC', async(req, res) => {
+    try {
+        const client = new MongoClient(process.env.MONGOLAB_URL)
+        const articles = await client.db("QCC-DB").collection("Articles").find({category: "CharlotteFC"}).sort({createdAt: -1}).toArray();
+        res.render('articles/CharlotteFC', { articles: articles, loggedIn: checkLoggedIn(req.user) })
+        await client.close()
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+router.get('/articles/:slug', async (req, res) => {
+    try {
+        const client = new MongoClient(process.env.MONGOLAB_URL)
+        const article = await client.db("QCC-DB").collection("Articles").findOne({ slug: req.params.slug });
+        if (article == null) { res.redirect('/') }
+        if (checkLoggedIn(req.user)) { res.render('articles/show', { article: article, loggedIn: true, name: (req.user.firstName + " " + req.user.lastName) }) }
+        else { res.render('articles/show', { article: article, loggedIn: false }) }
+        await client.close()
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+router.post('/comment', async (req, res) => {
+    const client = new MongoClient(process.env.MONGOLAB_URL)
+
+    try {
+        await client.db("QCC-DB").collection("Articles").findOneAndUpdate({ slug: req.body.slug }, {$push:{'comments':{'time': Date.now(), 'name': req.body.name, 'comment': req.body.comment}}})
+        await client.close()
+        
+        res.redirect('articles/' + req.body.slug)
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+function checkLoggedIn(user) {
+    if (user) return true
+    else return false
+}
 
 module.exports = router
