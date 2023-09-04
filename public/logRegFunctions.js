5d57e934dd7b7fc885b3b05c2996df3fb46a43c6
Cooper-Adams <cooperadamsbusiness@gmail.com>
2023-09-04 19:07:34 -0400

    Fixed the file throwing errors if unneeded elements weren't found as well as adding the password visibility toggle functionality

--- public/logRegFunctions.js
+++ public/logRegFunctions.js
@@ -2,15 +2,36 @@ const signUpButton = document.getElementById('signUp')
 const signInButton = document.getElementById('signIn')
 const container = document.getElementById('container')
 const errorDiv = document.querySelector('.error-div')
+const pwToggle = document.getElementById('pw-toggle')
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active")
-})
+if (signUpButton) {
+	signUpButton.addEventListener('click', () => {
+		container.classList.add("right-panel-active")
+	})
+}
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active")
-})
+if (signInButton) {
+	signInButton.addEventListener('click', () => {
+		container.classList.remove("right-panel-active")
+	})
+}
 
-setTimeout(function() {
-	errorDiv.classList.add('hidden')
-}, 2000);
+if (errorDiv) {
+	setTimeout(function() {
+		errorDiv.classList.add('hidden')
+	}, 2000)
+}
+
+if (pwToggle) {
+	pwToggle.addEventListener("click", () => {
+		const input = document.querySelector('input')
+
+		console.log(input)
+	
+		if (input.type == 'password') {
+			input.type = 'text'
+		} else {
+			input.type = 'password'
+		}
+	})
+}
